a3b89b7a49ff2f1d2773b362c82edf317b9ab071
Max Waitzman <vaymaks@gmail.co.il>
2016-03-08 19:40:56 +0200

    added Parcelable to User, Rescue , Car , Analytics

--- app/src/main/java/max/waitzman/oopshelpme/models/Car.java
+++ app/src/main/java/max/waitzman/oopshelpme/models/Car.java
@@ -1,12 +1,15 @@
 package max.waitzman.oopshelpme.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by User7 on 06/03/2016.
  */
-public class Car {
-	private String brand;
-	private String model;
-	private int year;
+public class Car implements Parcelable {
+	private String brand="";
+	private String model="";
+	private int year=1946;
 
 	public Car(){
 	}
@@ -45,4 +48,33 @@ public class Car {
 	public String toString() {
 		return  getBrand()  + " " + getModel() + " " + getYear() ;
 	}
+
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(this.brand);
+		dest.writeString(this.model);
+		dest.writeInt(this.year);
+	}
+
+	protected Car(Parcel in) {
+		this.brand = in.readString();
+		this.model = in.readString();
+		this.year = in.readInt();
+	}
+
+	public static final Parcelable.Creator<Car> CREATOR = new Parcelable.Creator<Car>() {
+		public Car createFromParcel(Parcel source) {
+			return new Car(source);
+		}
+
+		public Car[] newArray(int size) {
+			return new Car[size];
+		}
+	};
 }
--- app/src/main/java/max/waitzman/oopshelpme/models/Rescue.java
+++ app/src/main/java/max/waitzman/oopshelpme/models/Rescue.java
@@ -1,11 +1,14 @@
 package max.waitzman.oopshelpme.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 
 /**
  * Created by User7 on 06/03/2016.
  */
-public class Rescue {
+public class Rescue implements Parcelable {
 	private Location location;
 	private Date date;
 	private User stuckUser;
@@ -57,4 +60,36 @@ public class Rescue {
 	public String toString() {
 		return   "Rescue "+stuckUser +" "+ date ;
 	}
+
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeParcelable(this.location, 0);
+		dest.writeLong(date != null ? date.getTime() : -1);
+		dest.writeParcelable(this.stuckUser, 0);
+		dest.writeParcelable(this.extractorUser, 0);
+	}
+
+	protected Rescue(Parcel in) {
+		this.location = in.readParcelable(Location.class.getClassLoader());
+		long tmpDate = in.readLong();
+		this.date = tmpDate == -1 ? null : new Date(tmpDate);
+		this.stuckUser = in.readParcelable(User.class.getClassLoader());
+		this.extractorUser = in.readParcelable(User.class.getClassLoader());
+	}
+
+	public static final Parcelable.Creator<Rescue> CREATOR = new Parcelable.Creator<Rescue>() {
+		public Rescue createFromParcel(Parcel source) {
+			return new Rescue(source);
+		}
+
+		public Rescue[] newArray(int size) {
+			return new Rescue[size];
+		}
+	};
 }
